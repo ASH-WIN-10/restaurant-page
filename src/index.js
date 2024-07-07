f789668edf27cb04db8763e7f95d89500bec2970
Ashvin <ashvinjangid10@gmail.com>
2024-07-07 18:24:16 +0530

    add logo in the header

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@ import loadHome from "./loadHome";
 import loadMenu from "./loadMenu";
 import loadContact from "./loadContact";
 import "./style.css";
+import Logo from "./logo.svg";
+
+function header() {
+	const logoDiv = document.querySelector("#logo");
+
+	const logoImg = new Image();
+	logoImg.src = Logo;
+	logoDiv.appendChild(logoImg);
+
+	const logoText = document.createElement("span");
+	logoText.textContent = "COFFEED";
+	logoDiv.appendChild(logoText);
+}
+
+header();
