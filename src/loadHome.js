9dd3f871a5512cb0cf9cb1dcbeef772fe52140d0
Ashvin <ashvinjangid10@gmail.com>
2024-07-09 17:59:49 +0530

    additional improvements to the header and home page

--- src/loadHome.js
+++ src/loadHome.js
@@ -1,11 +1,15 @@
 import Coffee from "./coffee.svg";
 
 function loadHome() {
+	const navHome = document.querySelector("#navHome");
+	navHome.classList.add("active");
+
 	const content = document.querySelector("#content");
 	const homePage = document.createElement("div");
 	content.appendChild(homePage);
 	homePage.id = "home";
 
+	// Hero
 	const heroDiv = document.createElement("div");
 	const heroText = document.createElement("span");
 	heroText.id = "heroText";
@@ -15,28 +19,36 @@ function loadHome() {
 	coffeeImg.src = Coffee;
 	heroDiv.append(heroText, coffeeImg);
 
-	homePage.appendChild(heroDiv);
+	// Container 1
+	const container1 = document.createElement("div");
+	container1.className = "container";
 
 	const text1 = document.createElement("span");
-	text1.textContent = "Did you not? Try ours👇";
-	homePage.appendChild(text1);
+	text1.textContent = "Did you not? Try ours 👉";
 
 	const orderBtn = document.createElement("button");
 	orderBtn.textContent = "Order Now";
 	orderBtn.className = "primaryBtn";
-	homePage.appendChild(orderBtn);
+	container1.append(text1, orderBtn);
 
-	// const sectionBreak = document.createElement("hr");
-	// homePage.appendChild(sectionBreak);
+	// Section Break
+	const sectionBreak = document.createElement("hr");
 
-	// const text2 = document.createElement("span");
-	// text2.textContent = "Or explore your taste in our menu!";
-	// homePage.appendChild(text2);
+	// Container 2
+	const container2 = document.createElement("div");
+	container2.className = "container";
 
-	// const exploreBtn = document.createElement("button");
-	// exploreBtn.textContent = "Explore";
-	// exploreBtn.className = "primaryBtn";
-	// homePage.append(exploreBtn);
+	const text2 = document.createElement("span");
+	text2.textContent = "Or explore your taste in our menu!";
+
+	const exploreBtn = document.createElement("button");
+	exploreBtn.textContent = "Explore";
+	exploreBtn.className = "primaryBtn";
+
+	container2.append(text2, exploreBtn);
+
+	// Appending elements in the homePage
+	homePage.append(heroDiv, container1, sectionBreak, container2);
 }
 
 export default loadHome();
